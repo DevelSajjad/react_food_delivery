53d86af195f7bcd42a8e0b912e5aeb89c24fe14c
DevelSajjad <sinigdho01@gmail.com>
2025-02-27 20:41:41 +0600

    free commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
 
   return (
     <div className='w-full bg-slate-200 min-h-screen p-8'>
-
       <Nav/>
 
       <Categories/>
@@ -19,6 +18,7 @@ function App() {
      <div className='w-full flex flex-wrap gap-5 px-5 justify-center items-center pt-8'>
         <Dish/>
      </div>
+
     </div>
     
   )
--- src/pages/Dish.jsx
+++ src/pages/Dish.jsx
@@ -7,13 +7,15 @@ function Dish()
         <>  
             {
                 food_items.map((item, index) => (
-                    <div className="w-[300px] h-[400px] bg-white p-4 rounded-lg flex flex-col gap-3 hover:border-2 border-green-400">
+                    <div className="w-[300px] h-[400px] bg-white p-4 flex flex-col gap-3 rounded-lg hover:border-2 border-green-400">
                         <div className="w-[100%] h-[60%] overflow-hidden rounded-lg">
                             <img src={item.food_image} className="object-cover" alt="" srcset="" />
                         </div>
+
                         <div>
                             <p className="font-bold text-lg" > {item.food_name} </p>
                         </div>
+
                         <div className="flex justify-between">
                             <div className="font-bold text-green-500">
                                 {'Tk '+ item.price}
@@ -22,6 +24,7 @@ function Dish()
                                 <FaLeaf/> <span>{item.food_type}</span>
                             </div>
                         </div>
+
                         <button className="w-full p-2 bg-green-400 rounded-lg font-bold hover:bg-green-200" >Add to Dish</button>
                     </div>
                 ))
